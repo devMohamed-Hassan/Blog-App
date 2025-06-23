908f4c740673a76626b50b1625971aa54b4edf78
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-06-23 07:05:49 +0300

    feat(api): user signup endpoint

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const express = require('express')
+const mysql2 = require('mysql2');
 const app = express()
 const port = 3000
+app.use(express.json())
+
+
+const connection = mysql2.createConnection({
+     host: "localhost",
+     port: 3306,
+     user: 'devMohamed',
+     password: 'Strongpassword@1234',
+     database: "blogApp"
+
+})
+
+app.post('/signup', (req, res, next) => {
+     const { firstName, lastName, email, password } = req.body;
+     const query = `
+     INSERT INTO
+          users(first_name,last_name,email,password)
+     VALUES
+          (?,?,?,?)
+     `
+     connection.execute(query, [firstName, lastName, email, password], (err, val) => {
+          if (err) {
+               res.json({ err })
+          } else {
+               res.json({ message: "Done", val })
+          }
+
+     })
+
+})
+
+
+
+connection.connect((err) => {
+     if (err) {
+          console.log("Connection Faild!", err);
+     }
+     else {
+          console.log("Connection Successfully");
+
+     }
+
+})
 
 
 
