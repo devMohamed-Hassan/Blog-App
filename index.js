38f0c12a735fca2c1d5e59417276f39f0c074e53
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-06-29 04:16:06 +0300

    feat(api): get all blogs endpoint

--- index.js
+++ index.js
@@ -219,6 +219,40 @@ app.patch('/update-blog/:id', (req, res, next) => {
 });
 
 
+app.get('/get-blogs', (req, res) => {
+     const query = `
+        SELECT
+            blog.id AS blog_id,
+            blog.title AS title,
+            blog.body AS body,
+            blog.user_id AS user_id,
+            user.id AS user_id,
+            CONCAT(user.first_name, " ", user.last_name) AS name,
+            FLOOR(DATEDIFF(NOW(), user.date_of_birth) / 365.25) AS age
+        FROM
+            blogs AS blog
+        LEFT JOIN
+            users AS user
+        ON
+            blog.user_id = user.id
+    `;
+
+     connection.execute(query, (err, data) => {
+          if (err) {
+               console.error('Database error:', err);
+               return res.status(500).json({ message: 'Internal server error' });
+          }
+
+          res.status(200).json({ blogs: data });
+     });
+});
+
+
+
+
+
+
+
 connection.connect((err) => {
      if (err) {
           console.log("Connection Faild!", err);
